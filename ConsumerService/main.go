2aa69c22a574e8f311d73b515c54063a61b8593e
sourav <sourav@hotcocoasoftware.com>
2021-12-23 12:36:07 +0530

    Simplified Example

--- ConsumerService/main.go
+++ ConsumerService/main.go
@@ -7,12 +7,6 @@ import (
 	"time"
 )
 
-//func failOnError(err error, msg string) {
-//	if err != nil {
-//		log.Fatalf("%s: %s", msg, err)
-//	}
-//}
-
 func main() {
 	println("calling rconnect")
 	conn := rConnect()

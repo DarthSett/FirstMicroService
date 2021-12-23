737a747ce62025b0f66fc898682fcc737904fda3
sourav <sourav@hotcocoasoftware.com>
2021-12-23 12:30:17 +0530

    Simplified Example

--- ScraperService/main.go
+++ ScraperService/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	scraper "github.com/FirstMicroservice/ScraperService/Scraper"
-	"github.com/FirstMicroservice/dump"
 	"github.com/streadway/amqp"
 	"log"
 	"time"
@@ -16,24 +13,14 @@ import (
 //	}
 //}
 
-
-
 func main() {
-	con := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true&multiStatements=true", "root", "password", "database", "3306", "mslinks")
-	db, err := sql.Open("mysql", con)
-	scraper.FailOnError(err,"Can't connect to db")
-	println("@@@@ DB Connected")
-	defer db.Close()
-	err = nil
-
-
 	println("calling rconnect")
-	conn:=rConnect()
+	conn := rConnect()
 	println("leaving rconnect")
 	defer conn.Close()
 
 	ch, err := conn.Channel()
-	scraper.FailOnError(err, "Failed to open a channel")
+	FailOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(
@@ -44,7 +31,7 @@ func main() {
 		false,   // no-wait
 		nil,     // arguments
 	)
-	scraper.FailOnError(err, "Failed to declare a queue")
+	FailOnError(err, "Failed to declare a queue")
 
 	msgs, err := ch.Consume(
 		q.Name, // queue
@@ -55,20 +42,13 @@ func main() {
 		false,  // no-wait
 		nil,    // args
 	)
-	scraper.FailOnError(err, "Failed to register a consumer")
-	println("calling rmigrate")
-	_ = rMigrate(db)
-	if err == nil {println("db migrated")}
-	scraper.FailOnError(err,"Can't migrate db")
+	FailOnError(err, "Failed to register a consumer")
 
 	forever := make(chan string)
 
 	go func() {
 		for d := range msgs {
-			log.Printf("Received a message: %s", d.Body)
-			if string(d.Body) == "CSV Uploaded" {
-				scraper.Scrape(db)
-			}
+			fmt.Printf("Received a message: %s", d.Body)
 		}
 	}()
 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
@@ -76,33 +56,20 @@ func main() {
 
 }
 
-func rConnect() *amqp.Connection{
+func rConnect() *amqp.Connection {
 
-	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
-	if err!=nil{
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
 		println(err.Error())
 		println("trying to rc")
-		time.Sleep(5 *time.Second)
+		time.Sleep(5 * time.Second)
 		return rConnect()
 	}
 
 	return conn
 }
-
-func rMigrate(db *sql.DB) int{
-
-	err := dump.MigrateDatabase(db)
+func FailOnError(err error, msg string) {
 	if err != nil {
-		println(err.Error())
-		println("trying to reMigrate")
-		time.Sleep(5 *time.Second)
-		return rMigrate(db)
+		println(msg + ": " + err.Error())
 	}
-
-	return 0
 }
-
-
-
-
-
